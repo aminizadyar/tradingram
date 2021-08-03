00c8b22fb3f3ce64c44ed4fa0e6b3d4748dd7503
chatrasham <aminizadyar6800@gmail.com>
2021-08-03 23:48:23 +0430

    test

--- api/views.py
+++ api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from api.stock import fetch
+#from api.stock import fetch
+from .models import Stock
 
 def stocks_last_price(request):
-    context = {"data" :fetch.get_stocks_last_price()}
+    #context = {"data" :fetch.get_stocks_last_price()}
+    qs = Stock.objects.all();
+
+    context = {'qs': qs}
     return render(request, 'api/stocks_last_price.html', context)
 
 
