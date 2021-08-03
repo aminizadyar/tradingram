d5c4af078af255eaa9d11fb6874512e8250821c3
chatrasham <aminizadyar6800@gmail.com>
2021-08-03 23:28:34 +0430

    change the stock last price fetch to update the stocks models

--- api/stock/fetch.py
+++ api/stock/fetch.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from api.models import Stock
 def get_stocks_last_price():
     stocks=['MSFT', 'AAPL' ,'GOOG','AMZN','FB','TSLA','BRK-A',
             'TSM','TCEHY','BABA','V','NVDA','005930.KS',
@@ -12,5 +13,18 @@ def get_stocks_last_price():
     stocks_last_price={}
     for symbol in stocks:
         stocks_last_price[symbol]=round(tickers.tickers[symbol].history(period="1m").iloc[0]['Close'],2)
-    return  stocks_last_price
+    return stocks_last_price
 
+
+def get_stocks_last_price2():
+
+    stocks = list(Stock.objects.all().values_list('ticker', flat=True))
+    tickers = yf.Tickers(stocks)
+
+
+    for symbol in stocks:
+        obj=Stock.objects.get(ticker=symbol)
+        obj.last_price=round(tickers.tickers[symbol].history(period="1m").iloc[0]['Close'],2)
+        obj.save()
+        print (symbol ,"----",Stock.objects.get(ticker=symbol).last_price,"-----",tickers.tickers[symbol].history(period="1m").iloc[0]['Close'])
+    return
