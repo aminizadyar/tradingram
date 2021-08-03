75db8f5de16f95a68b5bb6e97b3b8fa5e6af9687
chatrasham <aminizadyar6800@gmail.com>
2021-08-03 23:51:04 +0430

    updated stock_last_price view to get the context variables from the model

--- api/stock/fetch.py
+++ api/stock/fetch.py
@@ -1,22 +1,15 @@
 import yfinance as yf
 from api.models import Stock
-def get_stocks_last_price():
-    stocks=['MSFT', 'AAPL' ,'GOOG','AMZN','FB','TSLA','BRK-A',
+
+stocks=['MSFT', 'AAPL' ,'GOOG','AMZN','FB','TSLA','BRK-A',
             'TSM','TCEHY','BABA','V','NVDA','005930.KS',
             'JPM','JNJ','LVMUY','WMT','UNH','MA','NSRGY','HD','PG',
             'RHHBY','BAC','PYPL','DIS','ASML','ADBE','CMCSA',
             'NKE','OR.PA','TM','KO','XOM','ORCL','PFE','CRM','LLY',
              'CSCO','VZ','NFLX','INTC','PEP','ABT','DHR']
 
-    tickers = yf.Tickers(stocks)
 
-    stocks_last_price={}
-    for symbol in stocks:
-        stocks_last_price[symbol]=round(tickers.tickers[symbol].history(period="1m").iloc[0]['Close'],2)
-    return stocks_last_price
-
-
-def get_stocks_last_price2():
+def get_stocks_last_price():
     stocks = list(Stock.objects.all().values_list('ticker', flat=True))
     tickers = yf.Tickers(stocks)
     for symbol in stocks:
--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-#from api.stock import fetch
 from .models import Stock
 
 def stocks_last_price(request):
-    #context = {"data" :fetch.get_stocks_last_price()}
-    qs = Stock.objects.all();
 
+    qs = Stock.objects.all();
     context = {'qs': qs}
     return render(request, 'api/stocks_last_price.html', context)
 
