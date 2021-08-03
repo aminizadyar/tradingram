e746317f9f737a213199242154a25201bbf30e3c
chatrasham <aminizadyar6800@gmail.com>
2021-08-03 13:53:29 +0430

    added a new model named stock and added it to admin site

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    ticker = models.CharField(max_length=30,unique=True)
+    last_price = models.IntegerField(default=0)
