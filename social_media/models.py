5c0f4f0535d39cf26e7bcdcfd600e0b6b31d74ae
chatrasham <aminizadyar6800@gmail.com>
2021-08-09 16:02:20 +0430

    compelelted the profile model

--- social_media/models.py
+++ social_media/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(null=True,blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
