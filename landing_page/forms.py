b9a839d2f4bf3e63230ddfca9cd5d45fb66df9ac
chatrasham <aminizadyar6800@gmail.com>
2021-08-09 11:51:05 +0430

    added a login form to the landing page

--- landing_page/forms.py
+++ landing_page/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class SignInForm(forms.Form):
+    username = forms.CharField(label='username', max_length=100, required=True,  widget=forms.TextInput(attrs={'placeholder': 'Enter Username'}))
+    password = forms.CharField(label='password', max_length=100,required=True,widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}) )
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
-from .forms import SignUpForm
+from .forms import SignInForm
 from django.contrib.auth import login, authenticate
 from django.shortcuts import redirect
+from django.forms import ValidationError
 
 
 def landing_page(request):
 
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('cryptocurrencies_last_price')
-    else:
-        form = SignUpForm()
+        form = SignInForm(request.POST)
+        username = request.POST.get('username')
+        raw_password = request.POST.get('password')
+        user = authenticate(username=username, password=raw_password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('cryptocurrencies_last_price')
+            else:
+                state = "Your account is not active, please contact the administrator."
+        else:
+            state= "Your username and/or password were incorrect."
 
-    return render(request, 'landing_page/index.html', {'form': form})
+    else:
+        form = SignInForm()
+        state = ""
+
+    return render(request, 'landing_page/index.html', {'form': form,'state':state})
 
 
 def registration_completed(request):
