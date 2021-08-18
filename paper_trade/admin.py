b0cfd3230d3bad5f10823b1befa7cd7b05217ae7
chatrasham <aminizadyar6800@gmail.com>
2021-08-18 15:52:15 +0430

    basic steps for match engine v2. defined the models

--- paper_trade/admin.py
+++ paper_trade/admin.py
@@ -3,8 +3,14 @@ from .models import Order
 from .models import OpenPositionOrder
 from .models import Portfolio
 from .models import Position
+from .models import OrderOpenPosition
+from .models import OrderClosePosition
+from .models import OpenPosition
 
 admin.site.register(Order)
 admin.site.register(OpenPositionOrder)
 admin.site.register(Portfolio)
 admin.site.register(Position)
+admin.site.register(OrderOpenPosition)
+admin.site.register(OrderClosePosition)
+admin.site.register(OpenPosition)
--- paper_trade/models.py
+++ paper_trade/models.py
@@ -2,14 +2,81 @@ from django.db import models
 from api.models import Symbol
 from django.contrib.auth.models import User
 
+class OrderOpenPosition(models.Model):
+    LONG = 1
+    SHORT = -1
+    DIRECTION_CHOICES = (
+        (LONG,'Long'),
+        (SHORT,'Short'),
+    )
+    RESULT_CHOICES = (
+        ('S', 'Success'),
+        ('F', 'Failure'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    input_price = models.FloatField(blank=False)
+    price_matched = models.FloatField(blank=True,null=True)
+    quantity = models.FloatField(blank=False)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    direction = models.IntegerField(choices=DIRECTION_CHOICES)
+    result = models.CharField(max_length=1,choices=RESULT_CHOICES,default='F')
+    leverage = models.IntegerField(default=1)
+    take_profit = models.FloatField(blank=True , null= True)
+    stop_loss = models.FloatField(blank=True,null=True)
 
-class Portfolio(models.Model):
+    def __str__(self):
+        return str(self.id) + " -- " + self.user.username + " -- " + self.symbol.symbol +  " -- " +str(self.created_datetime) +"--" + self.get_result_display()+ "--" +self.get_direction_display() +"  Order"
+
+class OrderClosePosition(models.Model):
+    LONG = 1
+    SHORT = -1
+    DIRECTION_CHOICES = (
+        (LONG,'Long'),
+        (SHORT,'Short'),
+    )
+    RESULT_CHOICES = (
+        ('S', 'Success'),
+        ('F', 'Failure'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    input_price = models.FloatField(blank=False)
+    close_position_price = models.FloatField(blank=True)
+    open_position_price = models.FloatField(blank=False)
+    quantity = models.FloatField(blank=False)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    direction = models.IntegerField(choices=DIRECTION_CHOICES)
+    result = models.CharField(max_length=1,choices=RESULT_CHOICES,default='F')
+    freed_margin = models.FloatField(blank=True)
+
+    def __str__(self):
+        return str(self.id) + " -- " + self.user.username + " -- " + self.symbol.symbol +  " -- " +str(self.created_datetime) +"--" + self.get_result_display()+ "--" +self.get_direction_display() +"  Order"
+
+
+
+class OpenPosition(models.Model):
+    LONG = 1
+    SHORT = -1
+    DIRECTION_CHOICES = (
+        (LONG, 'Long'),
+        (SHORT, 'Short'),
+    )
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
     quantity = models.FloatField(blank=False)
+    direction = models.IntegerField(choices=DIRECTION_CHOICES)
+    matched_price = models.FloatField(blank=False)
+    blocked_margin = models.FloatField(blank=False,null=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    take_profit = models.FloatField(blank=True, null=True)
+    stop_loss = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-        return self.user.username + " --- " + self.symbol.symbol + " --- Portfolio"
+        return str(self.id) + " -- " + self.user.username + " --- " + self.symbol.symbol + " quantity= " + str(self.quantity) + " open price= " + str(self.matched_price) + " direction= " + self.get_direction_display()
 
 class Position(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,6 +87,14 @@ class Position(models.Model):
 
     def __str__(self):
         return self.user.username + " --- " + self.symbol.symbol + " --- Portfolio"
+
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    quantity = models.FloatField(blank=False)
+
+    def __str__(self):
+        return  self.user.username + " --- " + self.symbol.symbol + " --- Portfolio"
 
 
 class Order(models.Model):
