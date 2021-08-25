14f7fb88873d998b60d039ce09e3bc42582a3d91
rev <alinajim@comp.nus.edu.sg>
2021-08-25 15:26:52 +0430

    added close position url

--- paper_trade/urls.py
+++ paper_trade/urls.py
@@ -6,4 +6,5 @@ from .views import markets_page
 urlpatterns = [
     path('markets', markets_page, name='markets_page'),
     path('markets/<slug:symbol>/', symbol_page, name='symbol_page'),
+    path('markets/<int:position_id>/', symbol_page, name='symbol_page'),
 ]
