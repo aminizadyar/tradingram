880a788c97cb9246527625148b1c6fc06a6b4a00
chatrasham <aminizadyar6800@gmail.com>
2021-08-28 14:44:32 +0430

    completed base2.html added titles extended all pages with base2.html

---
+++ static/base/js/sidebar.js
@@ -0,0 +1,20 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      $("#searchbar_base").attr("margin-inline-start","1100");
+  });
+
+})(jQuery);
