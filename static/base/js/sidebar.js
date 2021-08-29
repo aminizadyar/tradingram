0776bec9780bf100725da8f9b93f4131e6626d2e
chatrasham <aminizadyar6800@gmail.com>
2021-08-29 10:40:38 +0430

    made the sidebar stay at it's place when scrolling ** solved a problem regarding sidebar collapse

--- static/base/js/sidebar.js
+++ static/base/js/sidebar.js
@@ -14,6 +14,14 @@
 
 	$('#sidebarCollapse').on('click', function () {
 		$('#sidebar').toggleClass('active');
+
+		if($('.invisible_behind_sidebar').css('display') == "none"){ // or this.value == 'volvo'
+      		$('.invisible_behind_sidebar').css('display','block');
+			}
+      	else {
+		$('.invisible_behind_sidebar').css('display','none');
+			}
+
 		if($('.searchbar_base').css('margin-inline-start') == "250px"){ // or this.value == 'volvo'
       		$('.searchbar_base').css('margin-inline-start',"110px");
 			}
