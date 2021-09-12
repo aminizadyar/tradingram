9670e027c1174ed7184b2b9599d4a270de9d9fc3
chatrasham <aminizadyar6800@gmail.com>
2021-09-12 12:37:22 +0430

    modified the algo_trade page.

--- algo_trade/urls.py
+++ algo_trade/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import algorithmic_trading_page
+from .views import algorithmic_trading_page, etf_optimizer_page
 
 urlpatterns = [
     path('algorithmic-trading', algorithmic_trading_page, name='algorithmic_trading_page'),
+    path('fund-of-funds-result', etf_optimizer_page, name='etf_optimizer_page'),
 ]
--- algo_trade/views.py
+++ algo_trade/views.py
@@ -3,10 +3,14 @@ from django.contrib.auth.decorators import login_required
 from landing_page.views import LOGIN_URL
 from algo_trade.ETF_intelligent_portfolio import temp
 
-
 @login_required(login_url=LOGIN_URL)
 def algorithmic_trading_page(request):
+    context = {}
+    return render(request, 'algo_trade/algorithmic_trading_page.html', context)
+
+@login_required(login_url=LOGIN_URL)
+def etf_optimizer_page(request):
     uri = temp()
     context = {'data': uri}
-    return render(request, 'algo_trade/algorithmic_trading_page.html', context)
+    return render(request, 'algo_trade/etf_optimizer_page.html', context)
 
